a222e9f82cfd6f3e84720bb1ff7fe0e6a4cff50d
Aaron Enyetu <aaronenyetu@gmail.com>
2024-01-16 13:37:42 -0800

    Added northern and southern hemisphere pictures and they are showing

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import northernPic from './image/NorthernHemisphere.png'
+import southernPic from './image/SouthernHemisphere.jpg'
 
-const HemisphereDisplay = (latitude) => {
+
+const HemisphereDisplay = ({ latitude }) => {
   console.log(latitude)
   const hemisphere = latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere'
+  const picture = latitude > 0 ? northernPic : southernPic;
   return (
     <div>
+    <img src={picture} alt=""/>
    {hemisphere}
     </div>
   )
